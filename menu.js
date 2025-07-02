a4cdb0dba80bbc9dc657d1603935d96e2052e973
anabelendomenech <domenechanaa@gmail.com>
2025-07-02 20:25:11 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".main-nav");
-
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+  const nav = document.querySelector(".main-nav ul");
+  hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 });
