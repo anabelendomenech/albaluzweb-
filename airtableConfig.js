1661ccc8b5278c5527e159346b45a8adda5c0e3a
anabelendomenech <domenechanaa@gmail.com>
2025-07-04 04:34:37 -0300

    Create airtableConfig.js

---
+++ airtableConfig.js
@@ -0,0 +1,7 @@
+const API_KEY = "patcGAP2NVZ9e5J4I.23eb2991fae9fcd9b425649f12e8e61773f02a327d344c37309982deeb1b6ee3";
+const BASE_ID = "appraIuHWdh5tA4FU";
+
+const headers = {
+  Authorization: `Bearer ${API_KEY}`,
+  "Content-Type": "application/json"
+};
