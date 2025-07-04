7b33d4c1ce6d3783c700fbe61efcc93329382e1f
anabelendomenech <domenechanaa@gmail.com>
2025-07-04 18:13:51 -0300

    Update airtableConfig.js

--- airtableConfig.js
+++ airtableConfig.js
@@ -1,10 +1,12 @@
-// airtableConfig.js (NO subir a GitHub)
-const API_KEY = "ppatv7PT1S00KcD2ee.c5453132c504a3c8da03a4d4d603dcc5567734fbf85fba088e6c2128bd9ec6e7";
+// airtableConfig.js
+const AIRTABLE_TOKEN = "TU_TOKEN_NUEVO_PRIVADO";
 const BASE_ID = "appraIuHWdh5tA4FU";
 
-const headers = {
-  Authorization: `Bearer ${API_KEY}`,
-  "Content-Type": "application/json"
+const endpoints = {
+  RESERVAS: "RESERVAS",
+  CLIENTAS: "CLIENTAS",
+  VESTIDOS: "VESTIDOS",
+  CHECKLIST: "CHECKLIST",
+  FINANZAS: "FINANZAS",
+  "HORARIOS DISPONIBLES": "HORARIOS DISPONIBLES"
 };
-
-export { API_KEY, BASE_ID, headers };
