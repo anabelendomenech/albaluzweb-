e2f592e1175f29af88ad076e35bac0df8beb5a1e
anabelendomenech <domenechanaa@gmail.com>
2025-07-04 21:37:12 -0300

    Update airtableConfig.js

--- airtableConfig.js
+++ airtableConfig.js
@@ -1,5 +1,13 @@
 // airtableConfig.js
+var airtable_api_key = readFromFile('./.env.dev', AIRTABLE_API_KEY)
+
 const airtableConfig = {
-  apiKey: "patJkwJavmMqqadFg.4d29fe3dfc98fdf81ce2cb8de76da8c4a13e895da077b677a025b82d9e5c3221";
+  apiKey: airtable_api_key;
   baseId: "appraIuHWdh5tA4FU"
 };
+
+
+// En archivo .env.dev
+//AIRTABLE_API_KEY=patJkwJavmMqqadFg.4d29fe3dfc98fdf81ce2cb8de76da8c4a13e895da077b677a025b82d9e5c3221
+
+//Agregar .env.dev a .gitignore
