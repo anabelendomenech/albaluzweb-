99d062c5f5375108ec86358df78df10ce0082e4b
anabelendomenech <domenechanaa@gmail.com>
2025-07-04 22:47:28 -0300

    Update airtableConfig.js

--- airtableConfig.js
+++ airtableConfig.js
@@ -2,6 +2,6 @@
 const AIRTABLE_API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
 
 const airtableConfig = {
-  apiKey: AIRTABLE_API_KEY;
+  apiKey: AIRTABLE_API_KEY ,
   baseId: "appraIuHWdh5tA4FU"
 };
