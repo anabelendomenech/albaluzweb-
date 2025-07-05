5f1ba897fd7a656b14e728cf5c39236dc3bd7b40
anabelendomenech <domenechanaa@gmail.com>
2025-07-04 23:35:54 -0300

    api key removed

--- airtableConfig.js
+++ airtableConfig.js
@@ -1,3 +1,2 @@
 export const AIRTABLE_API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
-// export const AIRTABLE_API_KEY = "patGNuUOQ9ySK4064.0f14b9757a086de475cb01c90da9812a2202808e1f637b4a7c94de4e3da841ea";
 export const BASE_ID = "appraIuHWdh5tA4FU";
