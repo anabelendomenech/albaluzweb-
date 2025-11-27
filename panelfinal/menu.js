5add97960cfbaeaf89e37bea1c8b35a03d13e657
anabelendomenech <domenechanaa@gmail.com>
2025-11-27 18:49:14 -0300

    Update menu.js

--- panelfinal/menu.js
+++ panelfinal/menu.js
@@ -8,12 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   side.innerHTML = `
     <h2 class="menu-title">ALBALUZ</h2>
-    <div class="menu-item" onclick="location.href='./index.html'">Dashboard</div>
+    <div class="menu-item" onclick="location.href='./index.html'">Inicio</div>
     <div class="menu-item" onclick="location.href='./alquileres.html'">Alquileres</div>
     <div class="menu-item" onclick="location.href='./clientes.html'">Clientes</div>
-    <div class="menu-item" onclick="location.href='./vestidos.html'">Vestidos</div>
+    <div class="menu-item" onclick="location.href='catalogo.html'">Vestidos</div>
     <div class="menu-item" onclick="location.href='./gastos.html'">Gastos</div>
-    <div class="menu-item" onclick="location.href='./rendimiento.html'">Rendimiento</div>
     <div class="menu-item" onclick="logout()" style="margin-top:30px; color:#a33">Cerrar sesión</div>
   `;
 });
