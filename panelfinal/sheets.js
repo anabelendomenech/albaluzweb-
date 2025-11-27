e037877d3231c2d79052b81e0a07594c901b694b
anabelendomenech <domenechanaa@gmail.com>
2025-11-27 19:30:43 -0300

    Update sheets.js

--- panelfinal/sheets.js
+++ panelfinal/sheets.js
@@ -1,4 +1,4 @@
-const API_URL = "";
+const API_URL = "https://script.google.com/macros/s/AKfycbxypuZYU4cctCcLuuFvKe6IjaT2xN4VoXwp2DdzSGA4fxnoWQozpOgGy9EfFTeGJYPc-w/exec";
 
 async function apiRequest(data) {
   const res = await fetch(API_URL, {
